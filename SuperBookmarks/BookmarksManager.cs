6a522f13e71bb4497ccb3bfd781872c109c41fc7
Néstor Soriano Vilchez <konamiman@stackoverflow.com>
2017-10-02 13:33:58 +0200

    When a line containing a bookmark is removed, the bookmark is removed as well.

--- SuperBookmarks/BookmarksManager.cs
+++ SuperBookmarks/BookmarksManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Differencing;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Tagging;
 
@@ -67,9 +68,57 @@ namespace Konamiman.SuperBookmarks
             {
                 viewsByFilename[fileName] = newView;
                 bookmarksByView[newView] = new List<Bookmark>();
-            }   
+            }
+
+            newView.TextBuffer.Properties.AddProperty("key", newView);
+            newView.TextBuffer.Changed += TextBufferChanged;
         }
-        
+
+        private void TextBufferChanged(object sender, TextContentChangedEventArgs eventArgs)
+        {
+            bool LineIsEntirelyContainedInChange(ITextSnapshotLine line, ITextChange change)
+            {
+                return (
+                    line.Start.Position >= change.OldPosition &&
+                    line.EndIncludingLineBreak.Position <= change.OldEnd) ||
+                
+                //When a blank line is deleted, the change actually reports
+                //that the line break *of the previous line* was deleted.
+                //This must be handled as a special case.
+
+                (change.OldText == "\r\n" && change.NewText == "" &&
+                    change.OldEnd == line.EndIncludingLineBreak.Position);
+            }
+
+            if (!eventArgs.Changes.IncludesLineChanges)
+                return;
+
+            var lineDeletionChanges = eventArgs.Changes.Where(ch => ch.LineCountDelta < 0).ToArray();
+            if (lineDeletionChanges.Length == 0)
+                return;
+
+            var lines = eventArgs.Before.Lines;
+            var buffer = (ITextBuffer) sender;
+            var view = buffer.Properties["key"] as ITextView;
+            var bookmarks = bookmarksByView[view];
+            foreach (var change in lineDeletionChanges)
+            {
+                var deletedLines = lines.Where(l => LineIsEntirelyContainedInChange(l, change));
+                var deletedLineNumbers = deletedLines.Select(l => l.LineNumber + 1).ToArray();
+
+                foreach (var deletedLineNumber in deletedLineNumbers)
+                {
+                    var matchingBookmark = bookmarks.SingleOrDefault(b => b.GetRow(buffer) == deletedLineNumber);
+                    if (matchingBookmark != null)
+                    {
+                        var tagger = buffer.Properties.GetOrCreateSingletonProperty(() => new SimpleTagger<BookmarkTag>(buffer));
+                        tagger.RemoveTagSpan(matchingBookmark.TrackingSpan);
+                        bookmarksByView[view].Remove(matchingBookmark);
+                    }
+                }
+            }            
+        }
+
         public void SetOrRemoveBookmarkInCurrentDocument()
         {
             var currentView = Helpers.GetTextViewForActiveDocument();
