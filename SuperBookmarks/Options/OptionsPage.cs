fe2ded77ee9e863b64eaba50821b070848f3565e
Néstor Soriano Vilchez <konamiman@stackoverflow.com>
2017-10-05 17:49:17 +0200

    Option changes now are committed only if the user clicks "Ok" in the VS settings dialog

--- SuperBookmarks/Options/OptionsPage.cs
+++ SuperBookmarks/Options/OptionsPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -12,7 +13,17 @@ namespace Konamiman.SuperBookmarks
     [Guid("BC1182FD-1E47-4616-BFC8-062683FB207A")]
     public class OptionsPage : DialogPage
     {
+        OptionsControl page;
+
         private bool deletingALineDeletesTheBookmark = true;
+        private bool optionsChanged = false;
+        private bool deletingALineDeletesTheBookmarkChanged = false;
+
+        public OptionsPage()
+        {
+            page = new OptionsControl();
+            page.Options = this;
+        }
 
         public bool DeletingALineDeletesTheBookmark
         {
@@ -23,23 +34,45 @@ namespace Konamiman.SuperBookmarks
             set
             {
                 deletingALineDeletesTheBookmark = value;
-                DeletingALineDeletesTheBookmarkChanged?.Invoke(this, EventArgs.Empty);
-                OptionsChanged?.Invoke(this, EventArgs.Empty);
+                deletingALineDeletesTheBookmarkChanged = true;
+                optionsChanged = true;
             }
         }
 
         public event EventHandler DeletingALineDeletesTheBookmarkChanged;
         public event EventHandler OptionsChanged;
 
-        protected override IWin32Window Window
+        protected override void OnApply(PageApplyEventArgs e)
         {
-            get
-            {
-                OptionsControl page = new OptionsControl();
-                page.Options = this;
-                page.Initialize();
-                return page;
-            }
+            if(deletingALineDeletesTheBookmarkChanged)
+                DeletingALineDeletesTheBookmarkChanged?.Invoke(this, EventArgs.Empty);
+
+            if(optionsChanged)
+                OptionsChanged?.Invoke(this, EventArgs.Empty);
+
+            SetAsUnchanged();
+            base.OnApply(e);
         }
+
+        protected override void OnActivate(CancelEventArgs e)
+        {
+            page.Initialize();
+            SetAsUnchanged();
+            base.OnActivate(e);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            SetAsUnchanged();
+            base.OnClosed(e);
+        }
+
+        private void SetAsUnchanged()
+        {
+            optionsChanged = false;
+            deletingALineDeletesTheBookmarkChanged = false;
+        }
+
+        protected override IWin32Window Window => page;
     }
 }
